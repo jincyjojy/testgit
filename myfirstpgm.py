eb6a6e1dace9180eceb4470d8b4d32df5ac9e720
jincyjojy <jincysusanthomas123@gmail.com>
2018-04-06 14:03:17 +0530

    Update myfirstpgm.py

--- myfirstpgm.py
+++ myfirstpgm.py
@@ -3,7 +3,7 @@ import numpy as np
 #defining training dataset
 trainX = np.linspace(-1, 1, 101)
 trainY = 3*trainX + np.random.randn(*trainX.shape)*0.33
-print("hello1")
+
 #defining placeholders
 X = tf.placeholder("float")
 Y = tf.placeholder("float")
@@ -12,8 +12,8 @@ w = tf.Variable(0.0, name="weight")
 y_model = tf.multiply(X, w)
 cost = tf.pow((Y - y_model), 2)
 train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
-print("hello2")
-#initialize variables and defne session
+
+#initialize variables and define a session
 init = tf.initialize_all_variables()
 with tf.Session() as sess:
 	sess.run(init)
@@ -21,7 +21,3 @@ with tf.Session() as sess:
 		for (x, y) in zip(trainX, trainY):
 			sess.run(train_op, feed_dict={X:x, Y:y})
 			print(sess.run(w))
-print("hello3")
-with tf.Session() as sess:
-	sess.run(init)
-	print(sess.run(w))
